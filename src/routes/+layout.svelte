02c9ff82e999d4f69a003a11444604572fac3d9e
Shishir <shishir0019@gmail.com>
2024-11-15 21:17:09 +0600

    make layout as design and added logo to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,7 @@
 	let { children } = $props();
 </script>
 
+<nav class="flex items-center justify-center text-3xl font-bold border-b p-5">
+	<img class="w-52" src="/logo.png" alt="">
+</nav>
 {@render children()}
